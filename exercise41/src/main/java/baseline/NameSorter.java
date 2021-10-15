d7634523b5df1ddbeb16792eaeff408888967045
Dustin Battles <dbattles2001@knights.ucf.edu>
2021-10-15 13:21:16 -0400

    first real commit

---
+++ exercise41/src/main/java/baseline/NameSorter.java
@@ -0,0 +1,16 @@
+package baseline;
+
+public class NameSorter {
+
+    //readNames(scanner and an array to hold the names)
+    //a thing that scans next lines until next line is empty,
+    //adding those names to the string array
+
+    //writeNames(name array) throws ioexception
+    //create a writer
+    //for i to size of names print string[i]
+    //close the file
+
+}
+
+
---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,18 @@
+package baseline;
+
+public class Solution41 {
+
+    //main
+
+    //scanner for input file
+
+    //array of strings
+
+    //call readNames
+
+    //Collections.sort(names, String.CASE_INSENSITIVE_ORDER)
+
+    //close input file
+
+    //try ioexception
+}
