894835e98e0c4b3e1a232a7fd62ec2c7ed19a94f
Dustin Battles <dbattles2001@knights.ucf.edu>
2021-10-15 16:00:55 -0400

    commit 4

--- exercise42/src/main/java/baseline/Employee.java
+++ exercise42/src/main/java/baseline/Employee.java
@@ -1,12 +1,30 @@
 package baseline;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Employee {
 
     //void writeNames(filename)
-    //try and open the file with a scanner
-    //while hasnextline
-        //String temp = nextLine
-        //split temp by "," saving to a string array size 3 for fn ln salary
-        //print
-    //catch file not found
+    void writeNames(){
+        //try and open the file with a scanner
+        try{
+            File file = new File("exercise42_input.txt");
+            Scanner input = new Scanner(file);
+
+            //while hasnextline
+            while(input.hasNextLine()){
+                //String temp = nextLine
+                //split temp by "," saving to a string array size 3 for fn ln salary
+                //print
+
+                String temp = input.nextLine();
+                String[] bigTemp = temp.split(",");
+                System.out.printf("%-17s%-17s%-17s%n", bigTemp[0], bigTemp[1], bigTemp[2]);
+            }
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+    }
 }
--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -1,13 +1,22 @@
 package baseline;
 
+import java.io.FileNotFoundException;
+
 public class Solution42 {
-
     //main throws file not found exception
+    public static void main(String[] args) throws FileNotFoundException {
+        //employee d = new employee
 
-    //employee d = new employee
+        //scanner input = from the file
+        //scratch this ^ i dont need a scanner in main
 
-    //scanner input = from the file
+        Employee d = new Employee();
 
-    //"last name first name salary"
-    //d.writeNames
+        //"last name first name salary"
+        //d.writeNames
+
+        System.out.printf("%-17s%-17s%-17s%n", "Last Name", "First Name", "Salary");
+        System.out.println("----------------------------------------");
+        d.writeNames();
+    }
 }
