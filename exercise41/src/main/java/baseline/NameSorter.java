300ea8d6706e0937bc465ba82cd3f18eee745021
Dustin Battles <dbattles2001@knights.ucf.edu>
2021-10-15 15:06:44 -0400

    pushing some stuff

--- exercise41/src/main/java/baseline/NameSorter.java
+++ exercise41/src/main/java/baseline/NameSorter.java
@@ -1,16 +1,36 @@
 package baseline;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class NameSorter {
 
     //readNames(scanner and an array to hold the names)
     //a thing that scans next lines until next line is empty,
     //adding those names to the string array
+    void readNames(Scanner input, ArrayList<String> names){
+        while(input.hasNextLine()){
+            names.add(input.nextLine());
+        }
+    }
 
     //writeNames(name array) throws ioexception
     //create a writer
     //for i to size of names print string[i]
     //close the file
+    void writeNames(ArrayList<String> names) throws IOException{
+        FileWriter output = new FileWriter("exercise41_output.txt");
+        output.write("Total of " + names.size() + " names\n");
+        output.write("----------\n");
 
+        for(String i : names){
+            output.write(i + "\n");
+        }
+
+        output.close();
+    }
 }
 
 
--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -1,18 +1,39 @@
 package baseline;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Dustin Battles
+ */
+
 public class Solution41 {
+    public static void main(String[] args) throws FileNotFoundException {
+        NameSorter d = new NameSorter();
 
-    //main
+        //scanner for input file
+        Scanner input = new Scanner(new File("exercise41_input.txt"));
 
-    //scanner for input file
+        //array of strings
+        ArrayList<String> names = new ArrayList<String>();
 
-    //array of strings
+        //call readNames
+        d.readNames(input, names);
 
-    //call readNames
+        Collections.sort(names, String.CASE_INSENSITIVE_ORDER);
 
-    //Collections.sort(names, String.CASE_INSENSITIVE_ORDER)
+        //close input file
+        input.close();
 
-    //close input file
-
-    //try ioexception
+        //catch io exception
+        try{
+            d.writeNames(names);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
