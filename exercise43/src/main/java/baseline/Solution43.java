2bd8d293085bc320092f6121c2304ebaccfe4c2e
Dustin Battles <dbattles2001@knights.ucf.edu>
2021-10-16 14:36:27 -0400

    i hate my life

--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -1,13 +1,37 @@
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution43 {
     public static void main(String[] args) {
         //initialize scanner and application
+        Scanner input = new Scanner(System.in);
+        WebsiteGenerator d = new WebsiteGenerator();
 
         //ask for name and author
+        System.out.println("What is the name of the website?");
+        String websiteName = input.next();
+
+        System.out.println("What is the authors name?");
+        String authorsName = input.next();
 
         //ask for css and javascript folder
+        System.out.println("Would you like a folder for CSS? (y/n)");
+        String css = input.next();
+
+        System.out.println("Would you like a folder for JavaScript? (y/n)");
+        String javascript = input.next();
 
         //call the websitegenerator
+        System.out.println("Created" + d.createWebsite(websiteName));
+        System.out.println("Created " + d.createHTML(websiteName, authorsName));
+
+        //if they chose either of the optional ones
+        if(css.equalsIgnoreCase("y")){
+            System.out.println("Created " + d.createCSSFolder(websiteName));
+        }
+        if(javascript.equalsIgnoreCase("y")){
+            System.out.println("Created " + d.createJavaScriptFolder(websiteName));
+        }
     }
 }
--- exercise43/src/main/java/baseline/WebsiteGenerator.java
+++ exercise43/src/main/java/baseline/WebsiteGenerator.java
@@ -1,9 +1,48 @@
 package baseline;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 public class WebsiteGenerator {
+    //I need a "path" first
+    String path = "src/main/java/baseline";
+    String directory = "";
+
     //create a thing to make a folder opening and closing a directory
+    public String createWebsite(String websiteName){
+        directory = path + websiteName;
+        File newFolder = new File(directory);
+
+        newFolder.mkdirs();
+        return directory;
+    }
 
     //optional js and css folders
+    public String createCSSFolder(String websiteName){
+        directory = path + websiteName + "/css";
+        return directory;
+    }
+    public String createJavaScriptFolder(String websiteName){
+        directory = path + websiteName + "/js";
+        return directory;
+    }
 
     //function that creates the actual HTML
+    public String createHTML(String websiteName, String author){
+        directory = path + websiteName + "/index.html";
+
+        String html = "<title>" + websiteName + "</title>\n<meta>" + author + "</meta>";
+
+        File file = new File(directory);
+
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(html);
+            writer.close();
+            return directory;
+        }catch(Exception e){
+            return "Directory is messed up";
+        }
+    }
 }
