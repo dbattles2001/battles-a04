2870dd3a540fb785444a9cba387e995b94916a04
Dustin Battles <dbattles2001@knights.ucf.edu>
2021-10-17 10:24:54 -0400

    blah blah blah

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,10 @@
+package baseline;
+
+public class Solution44 {
+    //i'm not too sure about this one but
+
+    //we need to intialize the gson thing
+
+    //create a try and catch block
+    //try to read the info in,
+}
---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,13 @@
+package baseline;
+
+public class Solution46 {
+    public static void main(String[] args) {
+        //create a scanner for the file
+
+        //for each time a word appears in the file save it to the list
+
+        //if the word duplicates add ++ to that word otherwise add it to the list
+
+        //print accordingly
+    }
+}
